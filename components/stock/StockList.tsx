b129bc6bf43ba77842dcc03ca2472cb619818469
FHuang5 <huangfrederic2002@gmail.com>
2026-05-01 01:46:15 +0200

    Phase 3a: wire freshness badge + refresh button into StockRow

--- components/stock/StockList.tsx
+++ components/stock/StockList.tsx
@@ -172,6 +172,9 @@ export default function StockList({ cards: initial, forSaleKeys, registered }: S
               onMoveToPokedexClick={() => setMoveToPokedexCard(g.head)}
               onSetCount={handleSetCount}
               busy={busyKey === g.key}
+              onPriceRefreshed={(updated) => {
+                setCards((prev) => prev.map((c) => (c.id === updated.id ? updated : c)));
+              }}
             />
           ))}
         </ul>
